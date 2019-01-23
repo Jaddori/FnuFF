eb2be6e38c356f30868b9d937ea9a2569b1abee9
Jaddori <ohlsson.niclas@gmail.com>
2019-01-23 22:05:28 +0100

    dev - Game - Got things up and running on the laptop. Created a test level.

--- FnuFF/FnuFF/graphics.cpp
+++ FnuFF/FnuFF/graphics.cpp
@@ -24,7 +24,8 @@ void Graphics::load()
 	assets.loadPack( "./assets/textures/pack01.bin" );
 	assets.loadPack( "./assets/textures/tools.bin" );
 	//lightmap.load( "./assets/levels/lightmap_test03.lvl_light.tga" );
-	lightmap.load( "./assets/levels/great_hall.lvl_light.tga" );
+	//lightmap.load( "./assets/levels/great_hall.lvl_light.tga" );
+	lightmap.load( "./assets/levels/lightmap_test04.lvl_light.tga" );
 	lightmap.upload();
 	
 	textShader.load( "./assets/shaders/font.vs", "./assets/shaders/font.gs", "./assets/shaders/font.fs" );
--- FnuFF/FnuFF/main.cpp
+++ FnuFF/FnuFF/main.cpp
@@ -105,7 +105,8 @@ int update( void* args )
 	//level.load( "./assets/levels/cross_section_slopes.lvl" );
 	//level.load( "./assets/levels/no_draw_export_test.lvl" );
 	//level.load( "./assets/levels/lightmap_test03.lvl" );
-	level.load( "./assets/levels/great_hall.lvl" );
+	//level.load( "./assets/levels/great_hall.lvl" );
+	level.load( "./assets/levels/lightmap_test04.lvl" );
 
 	Ray ray;
 	Triangle triangle;
